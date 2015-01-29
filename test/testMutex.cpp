8b1588681f105f81fd04ecb8e24e20b8b3420033
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2015-01-29 16:56:41 +0800

    add testMutex.cpp

---
+++ test/testMutex.cpp
@@ -0,0 +1,29 @@
+// =====================================================================================
+// 
+//       Filename:  testMutex.cpp
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  2015年01月29日 16时45分41秒
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+//        Company:  Class 1204 of Computer Science and Technology
+// 
+// =====================================================================================
+
+#include <iostream>
+#include "../Muntex.h"
+
+using namespace liunian;
+	
+int main(int argc, char *argv[])
+{
+	
+
+
+	return 0;
+}
+}
