e4d80a2ee5b440ddca47cb2863f30a48850b78ae
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2015-02-08 00:25:41 +0800

    add Epoll

---
+++ Epoller.cpp
@@ -0,0 +1,25 @@
+// =====================================================================================
+// 
+//       Filename:  Epoller.h
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  2015年01月28日 00时11分58秒
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+//        Company:  Class 1204 of Computer Science and Technology
+// 
+// =====================================================================================
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+
+	return 0;
+}
+
---
+++ Epoller.h
@@ -0,0 +1,43 @@
+// =====================================================================================
+// 
+//       Filename:  Epoller.h
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  2015年01月28日 00时11分58秒
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+//        Company:  Class 1204 of Computer Science and Technology
+// 
+// =====================================================================================
+
+#ifndef EPOLLER_H
+#define EPOLLER_H
+
+#include <iostream>
+#include <sys/epoll.h>
+#include <vector>
+
+#include "EventLoop.h"
+#include "Channel.h"
+
+using std::vector;
+
+namespace liunian{
+class Epoll{
+	public:
+		Epoll(EventLoop *loop);
+		~Epoll();
+
+
+		bool updateChannel(Channel *channel);
+	//	bool removeChannel(Channel *channel);
+	private:
+		int epollFd;
+		struct epoll_event events[];
+}
+}
+#endif
