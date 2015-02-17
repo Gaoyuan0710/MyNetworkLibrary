219fb1af85a3f0b5cb151709463df5fae8c54587
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2015-02-17 16:23:46 +0800

    简单封装socket操作

---
+++ Socket.h
@@ -0,0 +1,44 @@
+// =====================================================================================
+// 
+//       Filename:  Socket.h
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  2015年02月17日 10时21分38秒
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+//        Company:  Class 1204 of Computer Science and Technology
+// 
+// =====================================================================================
+
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include <iostream>
+#include "InetAddress.h"
+
+namespace liunian{
+class Socket{
+	public:
+		Socket()
+		{
+			socketFd = creatSocket();
+		}
+		~Socket();
+		int getSocketFd() const{
+			std::cout << "get socket fd = " << socketFd << std::endl;
+			return socketFd;
+		}
+		int creatSocket();
+		
+		void Bind(const InetAddress &);
+		void Listen();
+		int Accept(InetAddress &);
+	private:
+	 	int socketFd;
+};
+}
+#endif
